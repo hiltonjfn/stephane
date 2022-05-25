b04fb06da19d0dc942c34bccf415265d6c01057b
hiltonjfn <88454528+hiltonjfn@users.noreply.github.com>
2022-05-25 00:29:54 -0300

    Add files via upload

---
+++ js/functions_html.js
@@ -0,0 +1,10 @@
+function encolher(){
+    var barra2 = document.getElementById("flush-collapseOne")
+    barra2.setAttribute('class','accordion-collapse collapse')
+}
+function crecer(){
+    var barra2 = document.getElementById("flush-collapseOne")
+    barra2.setAttribute('class','accordion-collapse collapse show')
+}
+
+
